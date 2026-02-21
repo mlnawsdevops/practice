0e1815e3a09bc9983b322f31fbbd0e22bd090f8e
mlnawsdevops <dev@123.email>
2026-02-21 17:12:43 +0530

    prac

--- root/parameter.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ssm_parameter" "sg" {
-  name = "/${project}/${environment}/sg"
-  type = "String"
-  value = module.sg.sg_id
-}
--- root/sg.tf
+++
@@ -1,7 +0,0 @@
-module "sg" {
-  source = "git::https://github.com/mlnawsdevops/practice.git?ref=main"
-  project = var.project
-  environment = var.environment
-  common_tags = var.common_tags
-  sg_tags = var.sg_tags
-}
